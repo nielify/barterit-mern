aba03fc7856df48880b229252e8f6ef1ef2b71ab
Niel Follero <nielfollero0420@gmail.com>
2021-06-18 23:12:54 +0800

    added polishInput middleware for polishing necessary registration data

---
+++ middlewares/polishInputs.js
@@ -0,0 +1,20 @@
+//removes extra spaces on start and end, 'Capitalize' each word, and
+const polish = (input) => {
+  return input
+    .trim()
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+};
+
+const polishRegInputs = (req, res, next) => {
+  req.body.lastName = polish(req.body.lastName);
+  req.body.firstName = polish(req.body.firstName);
+  req.body.middleName = polish(req.body.middleName);
+  req.body.specificAddress = polish(req.body.specificAddress);
+  console.log('polishing inputs');
+  console.log(req.body);
+}
+
+module.exports = polishRegInputs;
