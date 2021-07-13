3b599d05bfd9e2e7c9c3832c9624a554598bcb70
nielify <nielfollero@gmail.com>
2021-07-13 20:51:36 +0800

    fixed the photo display

--- client/src/Components/CreatePost/AddPhotos.js
+++ client/src/Components/CreatePost/AddPhotos.js
@@ -4,25 +4,20 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 import AddPhotoAlternateOutlinedIcon from '@material-ui/icons/AddPhotoAlternateOutlined';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-    padding: theme.spacing(1),
-    marginBottom: theme.spacing(2),
     border: 'solid 1px #bbb',
+    padding: theme.spacing(.5),
+    marginBottom: theme.spacing(2),
     borderRadius: 5,
   },
   card: {
-    width: 150,
-    maxWidth: 150,
-    height: 100,
+    height: '100%',
     border: 'solid 1px #bbb',
-    margin: theme.spacing(.5),
   },
   buttonActionArea: {
     display: 'flex',
@@ -37,78 +32,93 @@ const AddPhotos = () => {
   const classes = useStyles();
 
   return (  
-    <div className={classes.root}>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardMedia 
-          component="img"
-          image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
-        />
-      </Card>
-      <Card className={classes.card}>
-        <CardActionArea className={classes.buttonActionArea}>
-          <AddPhotoAlternateOutlinedIcon color="primary" />
-          <Typography
-            variant="subtitle1"
-          >
-            Add Photo
-          </Typography>
-        </CardActionArea>
-      </Card>
-    </div>
+    <Grid container spacing={1} className={classes.root}>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={3} sm={2}>
+        <Card>
+          <CardMedia 
+            component="img"
+            image="https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+          />
+        </Card>
+      </Grid>
+      
+      <Grid item xs={3} sm={2}>
+        <Card className={classes.card}>
+          <CardActionArea className={classes.buttonActionArea}>
+            <AddPhotoAlternateOutlinedIcon color="primary" />
+            <Typography
+              variant="subtitle1"
+            >
+              Add Photo
+            </Typography>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
  
