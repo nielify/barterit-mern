0b56078d8e05aea1347da4025cc21c18a5ac6d77
nielify <nielfollero@gmail.com>
2021-08-17 20:45:56 +0800

    make it better

--- client/src/TestComponents/signup/Signup.js
+++ client/src/TestComponents/signup/Signup.js
@@ -107,7 +107,7 @@ const Signup = ({ setShowProgress }) => {
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ confirmPassword, setConfirmPassword ] = useState('');
-  const [ captcha, setCaptcha] = useState(false);
+  const [ captcha, setCaptcha] = useState(true); //off on during test production
   const [ isChecked, setIsChecked] = useState(false);
   const [ isTownSelected, setIsTownSelected ] = useState(false);
   
