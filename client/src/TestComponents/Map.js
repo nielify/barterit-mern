8dc381d113c4b58578755821c00cd7577c84f08d
nielify <nielfollero@gmail.com>
2021-08-28 16:22:11 +0800

    added test map

--- client/src/TestComponents/Map.js
+++ client/src/TestComponents/Map.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -11,16 +12,27 @@ const useStyles = makeStyles((theme) => ({
 const Map = () => {
   useRemoveCover();
   const classes = useStyles();
+  
+  const [position, setPosition] = useState([0,0]);
 
+  useEffect(() => { 
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setPosition([pos.coords.latitude, pos.coords.longitude])
+      /* console.log(pos.coords); */
+      /* setPosition([pos.coords.latitude, pos.coords.longitude]); */
+      /* console.log(pos.coords.latitude);
+      console.log(pos.coords.longitude); */
+    }, error => console.log(error))
+  }, [position]);
 
   return (
     <Container maxWidth="md">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={[14.609053699999997, 121.02225650000001]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={position}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
