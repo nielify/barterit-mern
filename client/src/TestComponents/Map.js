ccf7e7105d2ba819abd0261f3fd3c7b99565bb38
nielify <nielfollero@gmail.com>
2021-08-29 15:28:52 +0800

    fixed some map codes and added auto center of map auto move of marker

--- client/src/TestComponents/Map.js
+++ client/src/TestComponents/Map.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import { makeStyles } from '@material-ui/core/styles';
 
 import Container from '@material-ui/core/Container';
@@ -15,19 +15,19 @@ const Map = () => {
   
   const [position, setPosition] = useState([0,0]);
 
-  useEffect(() => { 
-    navigator.geolocation.getCurrentPosition((pos) => {
-      setPosition([pos.coords.latitude, pos.coords.longitude])
-      /* console.log(pos.coords); */
-      /* setPosition([pos.coords.latitude, pos.coords.longitude]); */
-      /* console.log(pos.coords.latitude);
-      console.log(pos.coords.longitude); */
-    }, error => console.log(error), { enableHighAccuracy: true })
-  }, [position]);
+  useEffect(() => {  
+    const myPosition = navigator.geolocation.watchPosition((pos) => {
+      setPosition([pos.coords.latitude, pos.coords.longitude]);  
+    }, error => console.log(error), { enableHighAccuracy: true });
+    
+    return () => {
+      navigator.geolocation.clearWatch(myPosition);
+    }
+  }, []);
 
   return (
     <Container maxWidth="md">
-      <MapContainer center={[14.609053699999997, 121.02225650000001]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={position} zoom={15} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -37,9 +37,17 @@ const Map = () => {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        <ChangeMapView coords={position} />
       </MapContainer>
     </Container>
   );
+}
+
+function ChangeMapView({ coords }) {
+  const map = useMap();
+  map.setView(coords, map.getZoom());
+
+  return null;
 }
 
 export default Map;
